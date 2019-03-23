6f0baa554c5af23e0e70b99e8c41bb3b0a9401d7
Han Lee <hanlee12@gmail.com>
2019-03-23 16:02:17 -0500

    add Modal for creating and updating account

--- client/src/components/Accounts/index.js
+++ client/src/components/Accounts/index.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import styled from 'styled-components'
+import { Button, Container, Modal, ModalBody, ModalHeader } from 'reactstrap'
 
 import AccountCard from './AccountCard'
 
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 20px;
+`
+
 class Accounts extends Component {
   state = {
-    accounts: []
+    accounts: [],
+    showModal: false,
   }
 
   async componentDidMount() {
@@ -13,22 +26,59 @@ class Accounts extends Component {
     this.setState({ accounts: result.data })
   }
 
+  createAccount = () => {
+    this.setState({
+      showModal: true
+    })
+  }
+
+  _toggleModal = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
   render() {
-    const { accounts } = this.state;
+    const { accounts, showModal } = this.state;
     console.log('## accounts', accounts)
     const renderAccounts = (
       (accounts.length > 0) ? (
         accounts.map((account, index) => <AccountCard key={index} account={account}/>)
       ) : (
-        <h2>No Accounts, please add your account</h2>
+        <h4>No Accounts, please add your account</h4>
       )
     )
 
     return (
-      <div className="container" style={{marginTop: 30}}>
-        <h2>Account Listing</h2>
+      <Container fluid style={{marginTop: 30}}>
+        <Modal
+          isOpen={showModal}
+          toggle={this._toggleModal}
+          className="big-modal"
+          fade={false}
+        >
+            <>
+              <ModalHeader toggle={this._toggleModal}>
+                Add Account
+              </ModalHeader>
+              <ModalBody style={{ backgroundColor: '#f0f0f0' }}>
+                <p>Form</p>
+              </ModalBody>
+            </>
+        </Modal>
+        <Header>
+          <h2>Account Listing</h2>
+          <Button
+            className="cta btn"
+            size={'md'}
+            color={'primary'}
+            onClick={this.createAccount}
+          >
+            Add Account
+          </Button>
+        </Header>
         {renderAccounts}
-      </div>
+      </Container>
     )
   }
 }
