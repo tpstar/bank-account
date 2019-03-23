9bebbbe31866b52417413eacd82999ba4b074203
Han Lee <hanlee12@gmail.com>
2019-03-23 09:36:44 -0500

    build basic account api

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+#   Character.create(name: 'Luke', movie: movies.first)
+Account.create(routing_number: "271070801", account_number: "0802839335", bank_address: "180 N Michigan Ave
+Chicago, IL 60601", bank_nickname: "Citi Chicago")
+
+Account.create(routing_number: "081904808", account_number: "0802839336", bank_address: "515 N LaSalle Dr
+Chicago, IL 60654", bank_nickname: "Bank of America Chicago")
